f0e238a3dea76df73a92610519d925888db521e1
Smaregg <35426113+SmartEgg527@users.noreply.github.com>
2020-03-16 22:02:12 +0800

    Basic code & resources
    Flock codes
    Boid prefab

---
+++ Assets/Scripts/Flock/Boid.cs
@@ -0,0 +1,158 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boid : MonoBehaviour
+{
+    #region Params
+    /// <summary>
+    /// View distance
+    /// </summary>
+    public float SenseRange = 10;
+
+    /// <summary>
+    /// Boid's view angle
+    /// </summary>
+    public float FeildOfView = 300;
+
+    /// <summary>
+    /// Boid's move speed
+    /// </summary>
+    public float MoveSpeed = 30;
+
+    /// <summary>
+    /// Boid's angular velocity (in angle)
+    /// </summary>
+    public float AngularVelocity = 1.5f;
+
+    /// <summary>
+    /// Is draw nearby boids
+    /// </summary>
+    public bool IsDrawGismoz = false;
+    #endregion
+
+    #region Properties
+    /// <summary>
+    /// The flock this boid belongs to
+    /// </summary>
+    private FlockController m_fcBelonging;
+    public FlockController Belonging { get { return m_fcBelonging; } set { m_fcBelonging = value; } }
+
+    /// <summary>
+    /// Other boids in sight
+    /// </summary>
+    private List<Boid> m_lsOtherBoidInSight;
+    public List<Boid> OtherBoidInSight { get { return m_lsOtherBoidInSight; } }
+
+    /// <summary>
+    /// Fly destination
+    /// </summary>
+    private Vector3 m_v3DstPoint = Vector3.zero;
+    public Vector3 DstPoint { get { return m_v3DstPoint; } set { m_v3DstPoint = value; } }
+    #endregion
+
+    #region Interface
+    /// <summary>
+    /// Is other boid in sight
+    /// </summary>
+    /// <param name="tBoid"> Other boid </param>
+    /// <returns> In sight or not </returns>
+    public bool IsInSight(Boid tBoid)
+    {
+        Vector3 v3Dir = tBoid.transform.position - transform.position;
+        if (v3Dir.magnitude > SenseRange)
+            return false;
+        if (Vector3.Angle(transform.forward, v3Dir) > FeildOfView / 2)
+            return false;
+        return true;
+    }
+    #endregion
+
+    #region Flock Three Elements + Fly Towards Target
+    private void FlyToTarget()
+    {
+        Vector3 dirTurnTo = m_v3DstPoint - transform.position;
+        float v3Angle = Vector3.Angle(dirTurnTo, transform.forward);
+        transform.forward = Quaternion.AngleAxis(v3Angle > AngularVelocity ? AngularVelocity : v3Angle, Vector3.Cross(transform.forward, dirTurnTo)) * transform.forward;
+    }
+
+    private void Seperation()
+    {
+        Vector3 dirTurnTo = Vector3.zero;
+        for (int i = 0; i < m_lsOtherBoidInSight.Count; i++)
+            dirTurnTo += transform.position - m_lsOtherBoidInSight[i].transform.position;
+        float v3Angle = Vector3.Angle(dirTurnTo, transform.forward);
+        transform.forward = Quaternion.AngleAxis(v3Angle > AngularVelocity ? AngularVelocity : v3Angle, Vector3.Cross(transform.forward, dirTurnTo)) * transform.forward;
+    }
+
+    private void Alignment()
+    {
+        Vector3 dirTurnTo = Vector3.zero;
+        for (int i = 0; i < m_lsOtherBoidInSight.Count; i++)
+            dirTurnTo += m_lsOtherBoidInSight[i].transform.forward;
+        float v3Angle = Vector3.Angle(dirTurnTo, transform.forward);
+        transform.forward = Quaternion.AngleAxis(v3Angle > AngularVelocity ? AngularVelocity : v3Angle, Vector3.Cross(transform.forward, dirTurnTo)) * transform.forward;
+    }
+
+    private void Cohesion()
+    {
+        Vector3 dirTurnTo = Vector3.zero;
+        for (int i = 0; i < m_lsOtherBoidInSight.Count; i++)
+            dirTurnTo += m_lsOtherBoidInSight[i].transform.position;
+        dirTurnTo /= m_lsOtherBoidInSight.Count;
+        float v3Angle = Vector3.Angle(dirTurnTo, transform.forward);
+        transform.forward = Quaternion.AngleAxis(v3Angle > AngularVelocity ? AngularVelocity : v3Angle, Vector3.Cross(transform.forward, dirTurnTo)) * transform.forward;
+    }
+    #endregion
+
+    #region Method
+    /// <summary>
+    /// Change boid's direction
+    /// </summary>
+    private void ChangeDir()
+    {
+        if (m_fcBelonging.FlyToPoint)
+            FlyToTarget();
+        if (m_fcBelonging.DoSeparate)
+            Seperation();
+        if (m_fcBelonging.DoAlign)
+            Alignment();
+        if (m_fcBelonging.DoCohere)
+            Cohesion();
+    }
+
+    /// <summary>
+    /// Boid move along with it's direction
+    /// </summary>
+    private void MoveForward()
+    {
+        transform.position += transform.forward * MoveSpeed * Time.deltaTime;
+    }
+    #endregion
+
+    #region UnityLifecycle
+    // Use this for initialization
+    void Start()
+    {
+        m_lsOtherBoidInSight = new List<Boid>();
+        MoveSpeed += Random.Range(-5, 5);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ChangeDir();
+        MoveForward();
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (IsDrawGismoz)
+        {
+            Gizmos.color = Color.green;
+            foreach (var boid in m_lsOtherBoidInSight)
+                Gizmos.DrawLine(transform.position, boid.transform.position);
+        }
+    }
+    #endregion
+}
---
+++ Assets/Scripts/Flock/FlockController.cs
@@ -0,0 +1,158 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlockController : MonoBehaviour
+{
+    #region Properties
+    /// <summary>
+    /// This flock's entity model
+    /// </summary>
+    public Boid EntityPrefab;
+
+    /// <summary>
+    /// Flock width
+    /// </summary>
+    public float FlockWidth = 10;
+
+    /// <summary>
+    /// Flock height
+    /// </summary>
+    public float FlockHeight = 10;
+
+    /// <summary>
+    /// Flock depth
+    /// </summary>
+    public float FlockDepth = 10;
+
+    /// <summary>
+    /// This flock's entity number
+    /// </summary>
+    public int BoidNumber = 0;
+
+    public bool FlyToPoint = true;
+
+    /// <summary>
+    /// Separation switch
+    /// </summary>
+    public bool DoSeparate = true;
+
+    /// <summary>
+    /// Alignment switch
+    /// </summary>
+    public bool DoAlign = true;
+
+    /// <summary>
+    /// Cohesion switch
+    /// </summary>
+    public bool DoCohere = true;
+    #endregion
+
+    #region Properties
+    /// <summary>
+    /// Flock center
+    /// </summary>
+    public Vector3 Position { get { return transform.position; } set { transform.position = value; } }
+
+    /// <summary>
+    /// All entities in this flock
+    /// </summary>
+    private List<Boid> m_lsBoids;
+
+    private Vector3 m_v3FlockMoveDstPos;
+
+    private float m_fMoveInternal = 5;
+    private float m_fMoveCoolDown = 0;
+    #endregion
+
+    #region Interface
+    /// <summary>
+    /// Randomly generate point inside flock
+    /// </summary>
+    /// <returns> Point generated </returns>
+    public Vector3 GenerateRandomPoint()
+    {
+        float fPointX = Random.Range(-1 * FlockWidth / 2, FlockWidth / 2);
+        float fPointY = Random.Range(-1 * FlockHeight / 2, FlockHeight / 2);
+        float fPointZ = Random.Range(-1 * FlockDepth / 2, FlockDepth / 2);
+
+        return new Vector3(Position.x + fPointX, Position.y + fPointY, Position.z + fPointZ);
+    }
+
+    /// <summary>
+    /// Is boid still in flock
+    /// </summary>
+    /// <param name="tBoid"> Boic </param>
+    /// <returns> Boid in flock or not </returns>
+    public bool Contain(Boid tBoid)
+    {
+        Vector3 v3LocalBoidPos = transform.worldToLocalMatrix * tBoid.transform.position;
+        if (v3LocalBoidPos.x > FlockWidth / 2 || v3LocalBoidPos.x < -FlockWidth / 2)
+            return false;
+        if (v3LocalBoidPos.y > FlockHeight / 2 || v3LocalBoidPos.y < -FlockHeight / 2)
+            return false;
+        if (v3LocalBoidPos.z > FlockDepth / 2 || v3LocalBoidPos.z < -FlockDepth / 2)
+            return false;
+        return true;
+    }
+    #endregion
+
+    #region Life Cycle
+    // Use this for initialization
+    void Start ()
+    {
+        /*********** Set position ************/
+        Position = Vector3.zero;
+        m_v3FlockMoveDstPos = GenerateRandomPoint();
+        m_fMoveInternal = Random.Range(1, 3);
+
+        /*********** Create boids ************/
+        m_lsBoids = new List<Boid>(BoidNumber);
+        for (int i = 0; i < BoidNumber; i++)
+        {
+            Boid etyChild = (Boid)Instantiate(EntityPrefab);
+            etyChild.Belonging = this;
+            etyChild.DstPoint = m_v3FlockMoveDstPos;
+            etyChild.transform.position = GenerateRandomPoint();
+            etyChild.transform.rotation = Quaternion.Euler(Random.Range(0, 360), Random.Range(0, 360), 0);
+
+            m_lsBoids.Add(etyChild);
+        }
+    }
+    
+    // Update is called once per frame
+    void Update ()
+    {
+        if (m_fMoveCoolDown < m_fMoveInternal)
+            m_fMoveCoolDown += Time.deltaTime;
+        else
+        {
+            m_fMoveCoolDown = 0;
+            m_fMoveInternal = Random.Range(1, 3);
+            m_v3FlockMoveDstPos = GenerateRandomPoint();
+            for (int i = 0; i < BoidNumber; i++)
+                m_lsBoids[i].DstPoint = m_v3FlockMoveDstPos;
+        }
+
+        for (int i = 0; i < BoidNumber; i++)
+        {
+            /********** Add in sight boids ***********/
+            m_lsBoids[i].OtherBoidInSight.Clear();
+            for (int j = 0; j < BoidNumber; j++)
+            {
+                if (i == j) continue;
+                if (m_lsBoids[i].IsInSight(m_lsBoids[j]))
+                    m_lsBoids[i].OtherBoidInSight.Add(m_lsBoids[j]);
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireCube(Position, new Vector3(FlockWidth, FlockHeight, FlockDepth));
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(m_v3FlockMoveDstPos, 0.5f);
+    }
+    #endregion
+}
